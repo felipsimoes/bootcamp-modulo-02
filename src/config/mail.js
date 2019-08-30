689cc3e009a049087362ecafe24e08447fc3d0a4
Felipe Castro <engfelipecastro@gmail.com>
2019-08-30 10:33:35 -0300

    notification and cancel appointment

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'c6796f647a3a86',
+    pass: '94482b2bbaf749',
+  },
+  default: {
+    from: 'Felipe Castro <noreply@gobarber.com>',
+  },
+};
